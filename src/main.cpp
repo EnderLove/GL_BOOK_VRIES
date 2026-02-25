8d8c6cb1b59690073e4c1e36a6486931ca3e2669
EnderLove <cocpowergameplays@gmail.com>
2026-02-24 21:48:50 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,17 @@
 
 #include <iostream>
 
-const int SCREEN_WIDTH  = 16 * 80;
-const int SCREEN_HEIGHT =  9 * 90;
+const int SCR_WIDTH  = 16 * 80;
+const int SCR_HEIGHT =  9 * 90;
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main(){
     glfwInit();
@@ -13,7 +21,7 @@ int main(){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
    
-    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TESTING WINDOW", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "TESTING WINDOW", NULL, NULL);
     if (window == NULL){
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -25,7 +33,21 @@ int main(){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+   
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    while (!glfwWindowShouldClose(window)){
+        
+        glClearColor(0.5f, 0.1f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
 
