c83c2415f2d8283ff6f8b8d8ba21cf3ad7db813c
EnderLove <cocpowergameplays@gmail.com>
2026-03-19 16:17:23 -0500

    keyboard input

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
-#include <iostream>
+const int SCR_SOURCE_WIDTH  = 1920;
+const int SCR_SOURCE_HEIGHT = 1080;
 
 const int SCR_WIDTH  = 16 * 80;
 const int SCR_HEIGHT =  9 * 90;
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height){
     glViewport(0, 0, width, height);
+    printf("RESIZE: %4d | %4d\n", width, height);
 }
 
 void processInput(GLFWwindow *window){
@@ -15,33 +18,42 @@ void processInput(GLFWwindow *window){
         glfwSetWindowShouldClose(window, true);
 }
 
+void processColorScreen(GLFWwindow *window, float *r, float *g, float *b){
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) { *r += 0.01f; if (*r > 1) *r = 0; }
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) { *g += 0.01f; if (*g > 1) *g = 0; }
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) { *b += 0.01f; if (*b > 1) *b = 0; }
+}
+
 int main(){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Core-profile setting
    
     GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "TESTING WINDOW", NULL, NULL);
     if (window == NULL){
-        std::cout << "Failed to create GLFW window" << std::endl;
+        printf("Failed to create GLFW window");
         glfwTerminate();
         return -1;
     }
+    //glfwSetWindowPos(window, SCR_SOURCE_WIDTH / 4, SCR_SOURCE_HEIGHT / 4); // TODO : Set the window at the center 
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        printf("Failed to initialize GLAD");
         return -1;
     }
    
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+    
+    float r, g, b = 0;
 
     while (!glfwWindowShouldClose(window)){
-        
-        glClearColor(0.5f, 0.1f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
         processInput(window);
+        processColorScreen(window, &r, &g, &b);
+        
+        glClearColor(r, g, b, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
