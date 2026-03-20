a7f0f0f76de7653f7e7897d48bbc202546b01c79
EnderLove <cocpowergameplays@gmail.com>
2026-03-19 20:43:34 -0500

    shader creation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,23 @@ const int SCR_SOURCE_HEIGHT = 1080;
 const int SCR_WIDTH  = 16 * 80;
 const int SCR_HEIGHT =  9 * 90;
 
+int success;       // DEBUG VALUE
+char infoLog[512]; // DEBUG BUFFER
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 fragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "    fragColor = vec4(0.0, 0.0, 0.0, 1.0);\n"
+                                   "}\0";
+
 void frameBufferSizeCallback(GLFWwindow *window, int width, int height){
     glViewport(0, 0, width, height);
     printf("RESIZE: %4d | %4d\n", width, height);
@@ -46,14 +63,67 @@ int main(){
    
     glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
     
-    float r, g, b = 0;
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    ///////////////////// LOG SHADER ////////////////////////
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+    /////////////////////////////////////////////////////////
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram (shaderProgram);
+    glUseProgram  (shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    ///////////////////// LOG SHADER ////////////////////////
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR::LINKING::SHADER::FAILED\n%s\n", infoLog);
+    }
+    /////////////////////////////////////////////////////////
+
+    float r = 0; float g = 0; float b = 0;
 
     while (!glfwWindowShouldClose(window)){
         processInput(window);
         processColorScreen(window, &r, &g, &b);
         
-        glClearColor(r, g, b, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(r, g, b, 1.0f);  // This functions is a state-setting func for "glClear()"
+        glClear(GL_COLOR_BUFFER_BIT); // State-using function 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
