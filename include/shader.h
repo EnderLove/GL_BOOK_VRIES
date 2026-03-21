60517d64e644a90841a6ae7829f953eccc3842ab
EnderLove <cocpowergameplays@gmail.com>
2026-03-21 14:42:24 -0500

    class Shader set

---
+++ include/shader.h
@@ -0,0 +1,24 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader{
+    public:
+        unsigned int ID; // Program ID
+        
+        Shader(const char *vertexPath, const char *fragmentPath);
+        void use();
+
+        // Utility uniform functions
+        void setBool (const std::string name, bool  value) const;
+        void setInt  (const std::string name, int   value) const;
+        void setFloat(const std::string name, float value) const;
+};
+
+#endif // !SHADER_H
