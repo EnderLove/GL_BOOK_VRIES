391233ab02763280a6938eaf918342c57b73d12f
EnderLove <cocpowergameplays@gmail.com>
2026-04-21 13:17:18 -0500

    finishing model class methods

--- include/model.h
+++ include/model.h
@@ -6,8 +6,11 @@
 #include <assimp/postprocess.h>
 
 #include "mesh.h"
+#include "stb_image.h"
 
 class Model{
+    private:
+        std::vector<Texture> textures_loaded;
     public:
         Model(char *path);
         void draw(Shader &shader);
@@ -20,7 +23,8 @@ class Model{
         void loadModel(std::string path);
         void processNode(aiNode *node, const aiScene *scene);
         Mesh processMesh(aiMesh *mesh, const aiScene *scene);
-        
+       
+        unsigned int textureFromFile(const char *path, const std::string directory);
         std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
 };
 
