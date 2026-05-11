eced61ddf2b1bf3152c0962539ba450d6c6c1ed2
EnderLove <cocpowergameplays@gmail.com>
2026-05-10 22:44:02 -0500

    Cleaned some random duplicated code

--- src/main.cpp
+++ src/main.cpp
@@ -172,32 +172,11 @@ int main(){
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
     // SHADER LOADING 
-    Shader globalShader("../shaders/globalVertexShader.glsl", "../shaders/globalFragmentShader1.glsl");
+    Shader globalShader("../shaders/globalVertexShader.glsl", "../shaders/globalFragmentShader1.glsl"); // TODO SHADER SHOULD BE CREATED INSIDE THE SCENE?
        
     glm::vec4 lightPos = glm::vec4(-5.0f, 2.0f, 0.0f, 1.0f); // changed from vec3 to vec4 to set the "w" component to change between direction and position
 
-    glm::vec3 atteFlashConfig = glm::vec3(1.0f, 0.04f, 0.115f);
-    glm::vec3 attenuationConfig = glm::vec3(1.0f, 0.04f, 0.115f);
-    glm::vec3 attenuationConfig1 = glm::vec3(1.0f, 0.04f, 0.115f);
-    glm::vec3 attenuationConfig2 = glm::vec3(1.0f, 0.04f, 0.115f);
-    glm::vec3 attenuationConfig3 = glm::vec3(1.0f, 0.04f, 0.115f);
-    glm::vec3 attenuationConfig4 = glm::vec3(1.0f, 0.04f, 0.115f);
-
-    float flashColorLight[4] = {1, 1, 1, 1};
-    float colorLightP0[4] = {1, 1, 1, 1};
-    float colorLightP1[4] = {1, 1, 1, 1};
-    float colorLightP2[4] = {1, 1, 1, 1};
-    float colorLightP3[4] = {1, 1, 1, 1};
-    
-    float colorLight[4] = {1, 1, 1, 1};
     float alphaBlendVal = 0;
-    bool cameraFlashlight = false;
-
-    bool show_demo_window = true;
-    bool pointLight1 = false;
-    bool pointLight2 = false;
-    bool pointLight3 = false;
-    bool pointLight4 = false;
 
     printf("%s\n", glGetString(GL_VERSION));
 
